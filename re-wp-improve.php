772f7949938b332f00f9b996a5647e3f685f8491
sk <sk@renegade-empire.com>
2014-11-18 14:29:14 +1100

    Initial commit, disable file editing

---
+++ re-wp-improve.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Renegade Empire - WordPress Improvements
+Plugin URI: http://www.renegade-empire.com/
+Version: 0.1
+Author: Renegade Empire
+Description: Adds key functions that our team use regularly on projects.
+*/
+
+
+// Disable file editing
+define('DISALLOW_FILE_EDIT', true);
+
+?>
